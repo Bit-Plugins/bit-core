a72b3ab8c6f95c31db31a4831f5926eb6f9dbe17
Robin Painter <lochlanpainter@gmail.com>
2025-04-29 23:35:31 +1000

    Check for Roles function added

--- bit-core/index.js
+++ bit-core/index.js
@@ -52,6 +52,18 @@ module.exports = {
         }
     },
 
+    check_for_role: function check_for_role(client, guild_id, role_id) {
+        if(this.check_for_guild(client, guild_id)) {
+            if(client.guilds.fetch(guild_id).roles.fetch(role_id)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    },
+
     add_intent: function add_intent(intent) {
         if(!intent) {
             this.log(0, "Bit: Core", true, "Intents not defined!")
